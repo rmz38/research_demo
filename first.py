120039ccb24d9289317e1e770f9f5a9c5d4d7641
Rose Zhou <rmz38@cornell.edu>
2020-03-09 17:39:44 -0400

    finished generating random objects and applying various forces tot hem

--- first.py
+++ first.py
@@ -9,30 +9,55 @@ import os, glob, random
 
 planeId = pb.loadURDF("plane.urdf")
 
-visualShapeId = pb.createVisualShape(
-    shapeType=pb.GEOM_MESH,
-    fileName='random_urdfs/000/000.obj',
-    rgbaColor=None,
-    meshScale=[0.1, 0.1, 0.1])
+def pad(num):
+    if(num < 10):
+        return "00" + str(num)
+    elif(num < 100):
+        return "0" + str(num)
+    else:
+        return str(num)
 
-collisionShapeId = pb.createCollisionShape(
-    shapeType=pb.GEOM_MESH,
-    fileName='random_urdfs/000/000_coll.obj',
-    meshScale=[0.1, 0.1, 0.1])
+num_objects = random.randint(2,5)
+object_multi_ids = [None] * num_objects 
+object_forces = [None] * num_objects
+object_positions = [None] * num_objects
 
-multiBodyId = pb.createMultiBody(
-    baseMass=1.0,
-    baseCollisionShapeIndex=collisionShapeId, 
-    baseVisualShapeIndex=visualShapeId,
-    basePosition=[0, 0, 1],
-    baseOrientation=pb.getQuaternionFromEuler([0, 0, 0]))
+def check_object_positions(vect):
+    for i in range(num_objects):
+        if(np.array_equal(vect, object_positions[i])):
+            check_object_positions([random.randint(0,2),random.randint(0,2),random.randint(0,2)])
+    return vect
+
+#generate random number of objects of any texture and shape
+for i in range(num_objects):
+    chosen_shape = pad(random.randint(0,999))
+
+    visualShapeId = pb.createVisualShape(
+        shapeType=pb.GEOM_MESH,
+        fileName='random_urdfs/' + chosen_shape + '/' + chosen_shape + '.obj',
+        rgbaColor=None,
+        meshScale=[0.1, 0.1, 0.1])
+
+    collisionShapeId = pb.createCollisionShape(
+        shapeType=pb.GEOM_MESH,
+        fileName='random_urdfs/' + chosen_shape + '/' + chosen_shape + '_coll.obj',
+        meshScale=[0.1, 0.1, 0.1])
+
+    object_multi_ids[i] = pb.createMultiBody(
+        baseMass=1.0,
+        baseCollisionShapeIndex=collisionShapeId, 
+        baseVisualShapeIndex=visualShapeId,
+        basePosition= check_object_positions([random.randint(0,3),random.randint(0,3),random.randint(0,3)]),
+        baseOrientation=pb.getQuaternionFromEuler([0, 0, 0]))
 
 
-texture_paths = glob.glob(os.path.join('dtd', '**', '*.jpg'), recursive=True)
-random_texture_path = texture_paths[random.randint(0, len(texture_paths) - 1)]
-textureId = pb.loadTexture(random_texture_path)
-pb.changeVisualShape(multiBodyId, -1, textureUniqueId=textureId)
-pb.setGravity(0, 0, -9.8)
+    texture_paths = glob.glob(os.path.join('dtd', '**', '*.jpg'), recursive=True)
+    random_texture_path = texture_paths[random.randint(0, len(texture_paths) - 1)]
+    textureId = pb.loadTexture(random_texture_path)
+    pb.changeVisualShape(object_multi_ids[i], -1, textureUniqueId=textureId)
+
+# pb.setGravity(0, 0, -9.8)
+pb.setGravity(0, 0, 0)
 
 viewMatrix = pb.computeViewMatrix(
     cameraEyePosition=[0, 0, 3],
@@ -52,19 +77,22 @@ video_written = False
 out = cv2.VideoCapture(0)
 while(True):
     pb.setRealTimeSimulation(1)
-    objectPos, objectOrn = pb.getBasePositionAndOrientation(multiBodyId)
+    objectPos, objectOrn = pb.getBasePositionAndOrientation(object_multi_ids[0])
     width, height, rgbImg, depthImg, segImg = pb.getCameraImage(
     width=224, 
     height=224,
     viewMatrix=viewMatrix,
     projectionMatrix=projectionMatrix)
-    pb.applyExternalForce(multiBodyId, -1, [0,50,0], [0,0,0], pb.WORLD_FRAME)
+    pb.applyExternalForce(object_multi_ids[0], -1, [0,50,0], [0,0,0], pb.WORLD_FRAME)
+    # for i in range(num_objects):
+    #     pb.applyExternalForce(object_multi_ids[i], -1, [0,50,0], [0,0,0], pb.WORLD_FRAME)
     
-    rgbImg = np.asarray(rgbImg)
-    rgbImg = Image.fromarray(rgbImg, 'RGBA')
-    video.append(rgbImg)
-    rgbImg = np.asarray(rgbImg)
-    out.write(rgbImg)
+    # rgbImg = np.asarray(rgbImg)
+    # rgbImg = Image.fromarray(rgbImg, 'RGBA')
+    # video.append(rgbImg)
+    # rgbImg = np.asarray(rgbImg)
+
+    # out.write(rgbImg)
     # if(counter > 500 & ~video_written):
     #     out.release()
     #     video_written = True
@@ -72,10 +100,6 @@ while(True):
     # rgbImg.save("videos/first/first" + str(counter) + ".png", "PNG")
     
     counter += 1
-    # out.write(rgbImg)
-    # if(counter == 90):
-    #     out.release()
-    
 
 
 # make sure to call step simulation
